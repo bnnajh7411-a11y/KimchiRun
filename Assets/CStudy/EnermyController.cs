f993450e421f4616c80ee47df761c4298a59d296
jh <bnnajh7411@gmail.com>
2026-03-27 15:16:12 +0900

    유니티3차시

--- Assets/CStudy/EnermyController.cs
+++ Assets/CStudy/EnermyController.cs
@@ -1,35 +1,14 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class EnermyController : MonoBehaviour
 {
-    string enemyName = "슬라임";
-    int hp = 50;
-
     void Start()
     {
+        
     }
 
     void Update()
     {
-        var key = Keyboard.current;
-
-        if (key.eKey.wasPressedThisFrame)
-        {
-            TakeDamage(15);
-        }
-    }
-
-    void TakeDamage(int damage)
-    {
-        hp -= damage;
-        if (hp <= 0)
-        {
-            Debug.Log("슬라임이 쓰러졌다!");
-        }
-        else
-        {
-            Debug.Log("현재 체력: "+hp);
-        }
+        
     }
 }
--- Assets/CStudy/PlayerController.cs
+++ Assets/CStudy/PlayerController.cs
@@ -1,60 +1,26 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private bool isJumping = false;
     void Start()
     {
+        /*
+        string[] enermies = new string[3];
+        enermies[0] = "슬라임";
+        enermies[1] = "고블린";
+        enermies[2] = "오크";
+        */
+
+        string[] enermies = {"슬라임", "고블린", "오크", "적"};
+
+        for (int i = 0; i < enermies.Length; i++)
+        {
+            Debug.Log(enermies[i]);
+        }
     }
 
     void Update()
     {
-        var key = Keyboard.current;
-
-        if (key.leftArrowKey.wasPressedThisFrame)
-        {
-            if (isJumping)
-            {
-                Debug.Log("공중에서는 점프할 수 없습니다!");
-            }
-            else
-            {
-                isJumping = true;
-                Debug.Log("왼쪽으로 점프!");
-            }
-        }
-
-        if (key.rightArrowKey.wasPressedThisFrame)
-        {
-            if (isJumping)
-            {
-                Debug.Log("공중에서는 점프할 수 없습니다!");
-            }
-            else
-            {
-                isJumping = true;
-                Debug.Log("오른쪽으로 점프!");
-            }
-        }
-
-        if (key.upArrowKey.wasPressedThisFrame)
-        {
-            if (isJumping)
-            {
-                Debug.Log("공중에서는 점프할 수 없습니다!");
-            }
-            else
-            {
-                isJumping = true;
-                Debug.Log("위로 점프!");
-            }
-        }
-
-        if (key.spaceKey.wasPressedThisFrame)
-        {
-            isJumping = false;
-            Debug.Log("착지!");
-        }
+        
     }
 }
