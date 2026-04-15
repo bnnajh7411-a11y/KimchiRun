850dd446230d4ca8649d7035daada408ad7eb58a
jh <bnnajh7411@gmail.com>
2026-04-15 16:21:18 +0900

    building spawn

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [Header("스폰 시간 설정")]
+    public float minSpawnTime = 2f;
+    public float maxSpawnTime = 4f;
+
+    [Header("생성할 건물")]
+    public GameObject[] buildingPrefabs;
+
+    private void OnEnable()
+    {
+        float randomTime = Random.Range(minSpawnTime, maxSpawnTime);
+        Invoke("Spawn", randomTime);
+    }
+
+    private void OnDisable()
+    {
+        CancelInvoke();
+    }
+
+    void Start()
+    {
+        MakeInstance();
+    }
+
+    void Spawn()
+    {
+        MakeInstance();
+
+        float randomTime = Random.Range(minSpawnTime, maxSpawnTime);
+        Invoke("Spawn", randomTime);
+    }
+
+    void MakeInstance()
+    {
+        GameObject randomBuilding = buildingPrefabs[Random.Range(0, buildingPrefabs.Length)];
+        Instantiate(randomBuilding, transform.position, Quaternion.identity);
+    }
+
+}
