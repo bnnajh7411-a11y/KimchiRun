411e9c1edd38d91c4eedde3058618b5b4ce5ee00
pill <bnnajh7411@gmail.com>
2026-04-24 13:37:24 +0900

    Background Change

--- Assets/Scripts/BackgroundScroll.cs
+++ Assets/Scripts/BackgroundScroll.cs
@@ -4,6 +4,10 @@ public class BackgroundScroll : MonoBehaviour
 {
     private Material material;
 
+    [Header("변경할 이미지")]
+    public Texture2D newBackgroundImage;
+    private bool isChanged = false;
+
     private void Start()
     {
         material = GetComponent<MeshRenderer>().material;
@@ -13,5 +17,15 @@ public class BackgroundScroll : MonoBehaviour
     {
         float scrollSpeed = GameManager.Instance.CalculateGameSpeed() / 10;
         material.mainTextureOffset += new Vector2(scrollSpeed * Time.deltaTime, 0);
+
+        // 점수가 100점 이상이 될 때 이미지를 한 번만 변경
+        if (!isChanged && GameManager.Instance.CalculateScore() >= 100)
+        {
+            if (newBackgroundImage != null)
+            {
+                material.mainTexture = newBackgroundImage;
+            }
+            isChanged = true;
+        }
     }
 }
